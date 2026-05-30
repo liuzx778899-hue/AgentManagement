8713369aa329e997d031122062a6f80318342776
liuzx8888 <liuzx8888@yeah.net>
2026-05-30 15:34:51 +0800

    fix: import workflow-management.css for proper page styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/tokens.css";
 import "./styles/layout.css";
 import "./styles/components.css";
 import "./styles/pages.css";
+import "./styles/workflow-management.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
